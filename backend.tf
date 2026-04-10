c7660f009b8c3dcbb4200ba363f5eab5b9196650
Olayinka Hazeez <olayinka@Olayinkas-MacBook-Air.local>
2026-04-10 08:06:48 +0100

    Refactor backend configuration in backend.tf to remove unnecessary options

--- backend.tf
+++ backend.tf
@@ -2,8 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "olayinka-test-bucket"
     key            = "github/oidc-demo.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
+    region         = "us-east-1" 
   }
 }
